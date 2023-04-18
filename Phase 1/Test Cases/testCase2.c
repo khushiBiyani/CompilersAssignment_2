f8fb5583a707eedd31ea0cf6f5f7e3c79e5b0f2d
Khooshrin Aspi Pithawalla <88196065+Khooshrin@users.noreply.github.com>
2023-04-19 00:36:57 +0530

    test cases upload

---
+++ Phase 1/Test Cases/testCase2.c
@@ -0,0 +1,11 @@
+int main(){
+  //Illegal identifier
+  int 2invalid;
+  //Identifier too long
+  int abcdefghijklmnopqrstuvwxyzabcdefgh;
+  int x , y;
+  //Illegeal character
+  y = x $ x;
+  /* Nested comments /*are not*/ allowed */
+  return 0; 
+}
